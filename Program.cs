5967c45858ff617654eeafaf3ac05fd781275610
Wladimir <vag@57tpu.ru>
2023-12-04 13:02:10 +0700

    Start Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using ConsoleApp1;
+
+class Program {
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        screen.zavr();
+    }
+        }
---
+++ screen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class screen
+    {
+        public static void zavr()
+        {
+
+            Console.WriteLine("Вот библиотека");
+        }
+    }
+}
